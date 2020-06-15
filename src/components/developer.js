aded0e3a91ada6d8fccb7e223a450ff388a475a6
russo <florian.carusso@gmail.com>
2020-06-15 11:30:48 +0200

    :construction: Add label and headline to developer section

--- src/components/developer.js
+++ src/components/developer.js
@@ -1,9 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Developer = ({ name }) => <p>{name}</p>
+const Developer = ({ label, headline, name }) => (
+  <>
+    <p>{name}</p>
+    <p>{headline}</p>
+    <p>{label}</p>
+  </>
+)
 
 Developer.propTypes = {
+  label: PropTypes.string.isRequired,
+  headline: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 }
 
