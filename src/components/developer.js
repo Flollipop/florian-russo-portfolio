6ffa30165c745501e361e2dd29fdf34976dabe7b
russo <florian.carusso@gmail.com>
2020-06-15 11:36:07 +0200

    :construction: Add developer picture from github

--- src/components/developer.js
+++ src/components/developer.js
@@ -1,8 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Developer = ({ label, headline, name }) => (
+const Developer = ({ label, headline, name, picture }) => (
   <>
+    <img src={picture} alt="new" />
     <p>{name}</p>
     <p>{headline}</p>
     <p>{label}</p>
@@ -13,6 +14,7 @@ Developer.propTypes = {
   label: PropTypes.string.isRequired,
   headline: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
 }
 
 export default Developer
