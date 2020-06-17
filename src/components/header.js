4ca0e0237c1421b0b5de5198c4bab62b5719c41f
russo <florian.carusso@gmail.com>
2020-06-17 17:59:58 +0200

    :lipstick::sparkles: Add header
    Use true value
    Style with tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,33 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+function HeaderButton({ label, href }) {
+  return (
+    <a
+      href={`#${href}`}
+      className="flex w-24 justify-center text-white hover:bg-gray-700 h-16 py-6 px-4 hover:border-solid border-blue-100 hover:border-b-2"
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      {label}
+    </a>
+  )
+}
+
+HeaderButton.prototype = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+}
+
+const Header = ({ siteTitle }) => (
+  <nav className="flex items-center justify-between flex-row bg-gray-800 sticky top-0">
+    <div className="flex px-4 items-center flex-shrink-0 text-white mr-6 hidden md:block">
+      <span className="font-semibold text-xl tracking-tight">{siteTitle}</span>
     </div>
-  </header>
+    <div className="text-sm w-full flex-grow flex h-16">
+      <HeaderButton label={"About"} href={"about"} />
+      <HeaderButton label={"Education"} href={"education"} />
+      <HeaderButton label={"Work"} href={"work"} />
+    </div>
+  </nav>
 )
 
 Header.propTypes = {
