4a5dcf3a7bd40c0ba3c367634d4ba936228fa752
russo <florian.carusso@gmail.com>
2020-06-22 16:18:24 +0200

    :wrench: Update gatsby project info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Florian Russo full stack portfolio`,
+    description: `I'm a full stack developer, I love working with react so this website is made with gatsby`,
+    author: `Florian Russo`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
