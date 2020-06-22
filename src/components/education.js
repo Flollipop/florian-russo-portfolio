390e2b6ef59da1e5cab78d16fb203538d645c0c8
russo <florian.carusso@gmail.com>
2020-06-22 14:28:30 +0200

    :sparkles: Use react-time-line in education section

--- src/components/education.js
+++ src/components/education.js
@@ -1,13 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { VerticalTimeline } from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
 import { SchoolItem } from "./schoolItem"
 
 const Education = ({ education }) => (
-  <>
-    {education.map(school => (
-      <SchoolItem {...school} key={school.institution + school.startDate} />
-    ))}
-  </>
+  <section className="bg-blue-100 flex-col flex">
+    <VerticalTimeline>
+      {education.map(school => (
+        <SchoolItem {...school} key={school.institution + school.startDate} />
+      ))}
+    </VerticalTimeline>
+  </section>
 )
 
 Education.propTypes = {
