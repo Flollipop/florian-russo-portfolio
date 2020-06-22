507fd9479af4d0e11fa483d2ba6504a7388edaeb
russo <florian.carusso@gmail.com>
2020-06-22 16:47:21 +0200

    :sparkles: Add section title in education section

--- src/components/education.js
+++ src/components/education.js
@@ -3,9 +3,11 @@ import PropTypes from "prop-types"
 import { VerticalTimeline } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
 import { SchoolItem } from "./schoolItem"
+import { SectionTitle } from "./sectionTitle"
 
 const Education = ({ education }) => (
   <section className="bg-blue-100 flex-col flex">
+    <SectionTitle title={"Education"} />
     <VerticalTimeline>
       {education.map(school => (
         <SchoolItem {...school} key={school.institution + school.startDate} />
