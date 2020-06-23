dc56578c289c59f1b1912c5772c08cbc5c77d347
russo <florian.carusso@gmail.com>
2020-06-23 12:29:46 +0200

    :sparkles: Use vertical timeline to format work item

--- src/components/workItem.js
+++ src/components/workItem.js
@@ -1,9 +1,37 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { VerticalTimelineElement } from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+import { MdWork, MdLocationOn } from "react-icons/md"
 
-export const WorkItem = ({ company }) => <p>{company}</p>
+export const WorkItem = ({
+  company,
+  position,
+  location,
+  summary,
+  startDate,
+  endDate,
+}) => (
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date={`${startDate} ${endDate}`}
+    iconStyle={{ background: "rgb(233,154,148)", color: "#fff" }}
+    icon={<MdWork />}
+  >
+    <h4 className="vertical-timeline-element-title">{company}</h4>
+    <h5 className="vertical-timeline-element-subtitle">{position}</h5>
+    <div className="flex flex-row text-gray-500">
+      <MdLocationOn className="self-center" />
+      <text>{location}</text>
+    </div>
+
+    <text className="vertical-timeline-element-body">{`${summary}\n`}</text>
+  </VerticalTimelineElement>
+)
 
 WorkItem.propTypes = {
   company: PropTypes.string.isRequired,
   startDate: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
 }
