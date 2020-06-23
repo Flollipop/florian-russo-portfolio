f5ad4b382d47915bed2b38de261d5cbd292d5b38
russo <florian.carusso@gmail.com>
2020-06-23 12:32:17 +0200

    :sparkles: Use timeline component in work

--- src/components/work.js
+++ src/components/work.js
@@ -1,13 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { VerticalTimeline } from "react-vertical-timeline-component"
+import { SectionTitle } from "./sectionTitle"
 import { WorkItem } from "./workItem"
 
 const Work = ({ work }) => (
-  <>
-    {work.map(job => (
-      <WorkItem {...job} key={job.company + job.startDate} />
-    ))}
-  </>
+  <section className="bg-blue-100 flex-col flex">
+    <SectionTitle title="Work" />
+    <VerticalTimeline>
+      {work
+        .sort((a, b) => Date.parse(a.startDate) - Date.parse(b.startDate))
+        .map(job => (
+          <WorkItem {...job} key={job.company + job.startDate} />
+        ))}
+    </VerticalTimeline>
+  </section>
 )
 
 Work.propTypes = {
