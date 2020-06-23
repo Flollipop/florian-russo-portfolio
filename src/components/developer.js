7e1ce177450c9cc1a804cca28ad5a4ed2e6b7089
russo <florian.carusso@gmail.com>
2020-06-23 19:30:12 +0200

    :fire: Rename profiles component file ta avoid case error

--- src/components/developer.js
+++ src/components/developer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profiles from "./Profiles"
+import Profiles from "./profiles"
 
 const Developer = ({ label, headline, name, picture, summary, profiles }) => (
   <section
