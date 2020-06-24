a54982dbfcbfe4cce2cc03ab76609742ae56107f
russo <florian.carusso@gmail.com>
2020-06-24 14:01:54 +0200

    :sparkles: Add section background component using main image

---
+++ src/components/backgroundSection.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+
+const BackgroundSection = ({ className, children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "mainImage.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={`#040e18`}
+        >
+          {children}
+        </BackgroundImage>
+      )
+    }}
+  />
+)
+
+export default BackgroundSection
